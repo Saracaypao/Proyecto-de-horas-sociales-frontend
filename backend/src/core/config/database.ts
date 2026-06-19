7d0aeb4b31df4374320d2f8324b4903abfe6a12f
Saracaypao <00376721@uca.edu.sv>
2026-06-19 03:11:56 -0600

    fix postgres ssl

--- backend/src/core/config/database.ts
+++ backend/src/core/config/database.ts
@@ -4,6 +4,12 @@ import { env } from './env.js';
 const sequelize = new Sequelize(env.databaseUrl, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default sequelize;
